41ded638af1227bfa6ca03356bcd30cd8726a40c
imevanc <charal.vag@gmail.com>
2022-04-07 12:04:20 +0100

    deleted unused files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import ArticlesBlock from "./components/ArticlesBlock";
